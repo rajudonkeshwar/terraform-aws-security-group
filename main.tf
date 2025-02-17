4654fb233db263c0b7187aa89fcb4d166f1c5d34
rajudonkeshwar <145920069+rajudonkeshwar@users.noreply.github.com>
2025-02-17 19:47:11 +0530

    removing the ipv6

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "main" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    # ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = merge(
